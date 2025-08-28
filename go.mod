38513e66188d0776d0ae8063df1c9bb20b21d0b1
harishkokcha91 <harish.kokcha@gmail.com>
2025-08-28 10:08:58 +0530

    Initial commit with JWT and password utilities

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/sanyukt/SanyuktAuthUtils
+
+go 1.23.0
+
+toolchain go1.24.6
+
+require (
+	github.com/golang-jwt/jwt/v4 v4.5.2
+	golang.org/x/crypto v0.41.0
+)
---
+++ utils/utils.go
@@ -0,0 +1,57 @@
+package utils
+
+import (
+	"errors"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
+	"golang.org/x/crypto/bcrypt"
+)
+
+var jwtSecret = []byte(os.Getenv("JWT_SECRET"))
+
+// GenerateToken creates a new JWT token for a user
+func GenerateToken(userId string) (string, error) {
+	claims := jwt.MapClaims{
+		"userid": userId,
+		"exp":    time.Now().Add(24 * time.Hour).Unix(),
+		"iat":    time.Now().Unix(),
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(jwtSecret)
+}
+
+// ValidateToken validates and parses a JWT token
+func ValidateToken(tokenString string) (jwt.MapClaims, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// Make sure the signing method is HMAC
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("unexpected signing method")
+		}
+		return jwtSecret, nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	// Extract claims
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		return claims, nil
+	}
+
+	return nil, errors.New("invalid token")
+}
+
+// HashPassword hashes a plain-text password
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(bytes), err
+}
+
+// CheckPasswordHash compares a hashed password with a plain password
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
